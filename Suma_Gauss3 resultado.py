f0bc0cf0fba555eeb99913af129a1b79d81b5350
Leeeeoooouuhhh <71265244+Leeeeoooouuhhh@users.noreply.github.com>
2020-09-24 12:57:49 +0200

    Add files via upload

---
+++ Suma_Gauss3 resultado.py
@@ -0,0 +1,23 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:\Users\Usuario\Desktop\Trabajos Segundo\Suma_Gauss3.py =====
+>>> 
+===== RESTART: C:\Users\Usuario\Desktop\Trabajos Segundo\Suma_Gauss3.py =====
+Introduce hasta que numero  8
+suma_acumulada=  2
+suma_acumulada=  6
+suma_acumulada=  12
+suma_acumulada=  20
+>>> 
+===== RESTART: C:\Users\Usuario\Desktop\Trabajos Segundo\Suma_Gauss3.py =====
+Introduce hasta que numero  8
+suma_acumulada=  0
+suma_acumulada=  2
+suma_acumulada=  2
+suma_acumulada=  6
+suma_acumulada=  6
+suma_acumulada=  12
+suma_acumulada=  12
+suma_acumulada=  20
+>>> 
