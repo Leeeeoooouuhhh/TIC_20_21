1cc6f871782109407d730cca93367e4b01a2bb28
Leeeeoooouuhhh <71265244+Leeeeoooouuhhh@users.noreply.github.com>
2020-09-28 13:46:41 +0200

    Add files via upload

---
+++ edad_media.py
@@ -0,0 +1,20 @@
+'''Escribe un progrqama que lea las edades de 10 alumnos
+y devuelva la edad del mayor, la edad del menor y la diferencia de edades entre
+el mayor y el menor'''
+def edad_media():
+    # edad_mayor
+    # edad_menor
+    # diferencia
+    suma_edades=0
+    for cont in range(1,11):
+        print "edad ",cont,":"
+        edad=input() #instruccion i/o
+        suma_edades = suma_edades + edad
+    print "La suma de las edades es ", suma_edades    
+
+edad_media()
+    
+
+    
+    
+    
