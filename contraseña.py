c12bbaa309f10e5061b862358dac2df76e836174
Leeeeoooouuhhh <71265244+Leeeeoooouuhhh@users.noreply.github.com>
2020-10-01 12:39:39 +0200

    Add files via upload

---
+++ contraseña.py
@@ -0,0 +1,12 @@
+'''Escribe un programa que genere '''
+def contrasena():
+    nombre=raw_input("Introduce el nombre: ")
+    apellido=raw_input("Introduce el apellido: ")
+    print nombre," ",apellido,"eres el impostor?"
+
+contrasena()
+
+    
+    
+        
+	
