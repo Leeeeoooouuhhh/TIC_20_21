f924755c2828b282789b5a26284069ed3a0406eb
Leeeeoooouuhhh <71265244+Leeeeoooouuhhh@users.noreply.github.com>
2021-01-07 12:49:28 +0100

    Add files via upload

---
+++ Semillita.cpp
@@ -0,0 +1,17 @@
+/*
+Números aleatorios
+
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(){
+	float puntuacion;
+	//int semillita;
+	//random(semillita);
+	puntuacion=rand();
+	printf("%f",puntuacion);
+	return 0;
+}
